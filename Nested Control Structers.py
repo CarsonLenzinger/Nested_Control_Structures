fab5148988ac7540e32cd03ed8d465eb5f531b66
Carson <02carson.lenzinger@gulllakecs.org>
2019-10-23 13:57:43 -0400

    Initial Commit - For Loop + While Loop

--- Nested Control Structers.py
+++ Nested Control Structers.py
@@ -10,3 +10,16 @@ for i in range(3):
     print("Outer For Loop: " + str(i))
     for l in range(2):
         print("     Inner For Loop: " + str(l))
+
+
+print("\n****************************\n")
+
+
+'''
+Programmer: Carson Lenzinger
+Date: 10.23.19
+Program: ForLoop + WhileLoop
+
+This program will create a For Loop with a While Loop embedded into it
+'''
+
